b930501140f42ad04e16d7f182a418b21aed18c3
leonid <kotovpetia@gmail.com>
2022-08-25 11:49:02 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './styles/App.css'
+
+function App() {
+
+    return (
+        <div className="App">
+            <div className="post">
+                <div className="post__content">
+                    <strong>1. Javascript</strong>
+                    <div>
+                        Javascript - programming language
+                    </div>
+                </div>
+                <div className="post__buttons">
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
